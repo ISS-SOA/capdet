cc11b77f5eb7aa65dd6c9e1aff41180e88e6fe13
Soumya Ray <soumya.ray@gmail.com>
2014-10-26 22:22:27 +0800

    capdet.rb: Working Cadet class

--- capdet.rb
+++ capdet.rb
@@ -1,11 +1,21 @@
 require 'open-uri'
 require 'json'
+require 'yaml'
 
-content = open('http://localhost:9292/api/v1/cadet/soumya.ray.json').read
-content
-js = JSON.parse(content)
-js
-js["id"]
-js["badges"]
-js["badges"][2]
-js["badges"].select {|badge| badge["id"] == "10 Exercises" }
+class Cadet
+  attr_reader :name, :type, :badges
+  def initialize(username)
+    cadet_json = open("https://codebadges.herokuapp.com/api/v1/cadet/#{username}.json").read
+    cadet_h = JSON.parse(cadet_json)
+
+    @name = cadet_h['id']
+    @type = cadet_h['type']
+    @badges = {}
+    cadet_h['badges'].each do |badge|
+      @badges[badge['id']] = badge['date']
+    end
+  end
+end
+
+cap = Cadet.new('soumya.ray')
+puts cap.badges.to_yaml
