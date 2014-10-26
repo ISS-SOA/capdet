037654d63b066d02fe216438dcbed7d5a5734f2c
Soumya Ray <soumya.ray@gmail.com>
2014-10-27 00:37:21 +0800

    Now handles JSON POST to /api/v1/check

--- capdet.rb
+++ capdet.rb
@@ -1,29 +1,60 @@
+#!/usr/bin/env ruby
+
 require 'open-uri'
+require 'httparty'
 require 'json'
 require 'yaml'
 
-##
-# This class captures information about a user returned codebadges.herokuapp.com
-class Cadet
-  attr_reader :name, :type, :badges
-
-  URL = 'https://codebadges.herokuapp.com/api/'
+module Capdet
+  API_URL = 'http://localhost:9292/api'
   VER = 'v1'
   EXT = 'json'
   RESOURCE = 'cadet'
+  ##
+  # This class captures information about a user returned codebadges.herokuapp.com
+  class Cadet
+    attr_reader :name, :type, :badges
 
-  def initialize(username)
-    cadet_json = open("#{URL}/#{VER}/#{RESOURCE}/#{username}.#{EXT}").read
-    cadet_h = JSON.parse(cadet_json)
+    def initialize
+    end
 
-    @name = cadet_h['id']
-    @type = cadet_h['type']
-    @badges = {}
-    cadet_h['badges'].each do |badge|
-      @badges[badge['id']] = badge['date']
+    def initialize(username)
+      get(username)
+    end
+
+    def get(username)
+      cadet_json = open("#{API_URL}/#{VER}/#{RESOURCE}/#{username}.#{EXT}").read
+      cadet_h = JSON.parse(cadet_json)
+
+      @name = cadet_h['id']
+      @type = cadet_h['type']
+      @badges = {}
+      cadet_h['badges'].each do |badge|
+        @badges[badge['id']] = badge['date']
+      end
+    end
+  end
+
+  ##
+  # Runs class level operations from codebadges.herokuapp.com
+  class Academy
+    def self.check(usernames, badges)
+      params_h = {
+        usernames: usernames,
+        badges: badges
+      }
+      url = "#{Capdet::API_URL}/#{Capdet::VER}/check"
+      options = { body: params_h.to_json,
+                  headers: { 'Content-Type' => 'application/json' }
+                }
+      response = HTTParty.post(url, options)
+      puts response
     end
   end
 end
 
-cap = Cadet.new('soumya.ray')
-puts cap.badges.to_yaml
+# cap = Cadet.new('soumya.ray')
+# puts cap.badges.to_yaml
+
+#Capdet::Academy.check(['soumya.ray', 'chenlizhan'],
+#                      ['Object-Oriented Programming II'])
